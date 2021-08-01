4ef53e42cd5d0832c9bbf22d9a67d9964f158499
tanmay <tanmaymondalmondal7@gmail.com>
2021-08-01 10:06:02 +0530

    done

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-
+import requests
 
 
 def index(request):
@@ -8,14 +8,40 @@ def index(request):
     return render(request, 'app/home.html')
 
 def theam(request,username):
-    if username == 'theamone':
 
-        return render(request, 'app/theamone.html')
-    if username == 'theamtwo':
-        return render(request, 'app/theamtwo.html')
-    if username == 'theamthree':
-        return render(request, 'app/theamthree.html')
-    if username == 'theamfour':
-        return render(request, 'app/theamfour.html')
-    else:
-        return render(request, 'app/error.html')
+    print("https://us-central1-instacooll.cloudfunctions.net/webdata/"+username)
+    response = requests.get("https://us-central1-instacooll.cloudfunctions.net/webdata/"+username)
+    print(response)
+    if response.status_code==200:
+        print(response.json())
+        data=response.json()
+        selectedteam=data['theme']
+        print(selectedteam)
+        Data={
+            'name':data['name'],
+            'dp':data['dp'],
+            'bio':data['bio'],
+            'fb':data['fb'],
+            'ista':data['insta'],
+            'youtube':data['youtube'],
+            'linkedin':data['linkedin'],
+            'twitter':data['twitter'],
+            'phone':data['phone'],
+            'whatsapp':data['whatsapp'],
+            'links':data['links'],
+            'count':len(data['links'])
+        }
+        print(len(data['links']))
+        if selectedteam == 0:
+
+            return render(request, 'app/theamone.html',Data)
+        if selectedteam == 1:
+            return render(request, 'app/theamtwo.html',Data)
+        if selectedteam == 2:
+            return render(request, 'app/theamthree.html',Data)
+        if selectedteam == 3:
+            return render(request, 'app/theamfour.html',Data)
+        else:
+            return render(request, 'app/error.html', Data)
+
+    return render(request, 'app/er.html')
